385de654af55f1c9798f53dd9549e923d8a03564
sibansu1211 <sibansughosh.cob@gmail.com>
2022-09-05 13:27:27 +0530

    first commit

---
+++ src/components/left/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './index.css';
+
+export default function Index() {
+  return (
+    <div className='left'>
+        <div className='o'>
+             <h1>O</h1>
+             <h2 className='text'>BASE <br/> APPAREL</h2>
+             
+        </div>
+             <div className='we'>
+                <h1>
+                WE’RE <br/>
+                COMING <br/>
+                SOON
+                </h1>
+            </div>
+            <div className='sample'>
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. <br/> Lorem psum has been the industry’s standard dummy Lorem Ipsum is <br/> simply dummy text of the printing and typesetting industry.
+            </div>
+            <div className='input-text'>
+                <input  placeholder='E-Mail Address'></input>
+
+            </div>
+            
+    </div>
+    
+  )
+}
+
---
+++ src/components/right/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import image from './image.png'
+
+export default function Right() {
+  return (
+    <div className='img2'>
+        <img src={image} alt="img"/>
+
+
+
+    </div>
+  )
+}
